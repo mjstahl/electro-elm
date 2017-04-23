855e69a7f07b7ab262086813d3bf4e04dc2080c1
Mark Stahl <mjstahl@gmail.com>
2017-04-23 13:59:19 -0400

    Linting. Add elm project stuff to .gitignore

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 'use strict'
-const electron = require('electron')
-const chokidar = require('chokidar') // add chokidar
 
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const chokidar = require('chokidar');
 
-let mainWindow
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
 
 app.on('ready', createWindow)
 
@@ -15,29 +16,29 @@ chokidar.watch(['ports.js', 'index.html', 'elm.js']).on('change', () => {
     if (mainWindow) {
         mainWindow.reload()
     }
-})
+});
 
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1024,
         height: 768
-    })
+    });
 
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
 
     mainWindow.on('closed', function () {
         mainWindow = null
-    })
+    });
 }
 
 /* Mac Specific things */
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') { app.quit() }
-})
+});
 
 app.on('activate', () => {
     if (mainWindow === null) { createWindow() }
-})
+});
